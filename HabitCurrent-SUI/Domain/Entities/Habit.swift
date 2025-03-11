f9a68577931b30a7320c36dcd625a41222c087c3
KsArT-IT <ksart.it@gmail.com>
2025-03-11 16:59:06 +0200

    feat: add Habit and User entities for domain model

---
+++ HabitCurrent-SUI/Domain/Entities/Habit.swift
@@ -0,0 +1,16 @@
+//
+//  Habit.swift
+//  HabitCurrent-SUI
+//
+//  Created by KsArT on 11.03.2025.
+//
+
+import Foundation
+
+struct Habit {
+    let id: String
+    let title: String
+    let created: Date
+    let closed: Date
+    let isActive: Bool
+}
---
+++ HabitCurrent-SUI/Domain/Entities/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  HabitCurrent-SUI
+//
+//  Created by KsArT on 11.03.2025.
+//
+
+import Foundation
+
+struct User {
+    let id: String
+    let name: String
+}
