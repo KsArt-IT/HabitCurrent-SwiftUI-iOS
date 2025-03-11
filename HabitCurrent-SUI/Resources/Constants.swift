758c294d94e7aedc5ac00d9f10a0116ff71f3906
KsArT-IT <ksart.it@gmail.com>
2025-03-11 16:56:49 +0200

    feat: add Constants enum for managing app-wide constants and dimensions

---
+++ HabitCurrent-SUI/Resources/Constants.swift
@@ -0,0 +1,40 @@
+//
+//  Constants.swift
+//  HabitCurrent-SUI
+//
+//  Created by KsArT on 11.03.2025.
+//
+
+import UIKit
+
+enum Constants {
+    // Название приложения из Bundle
+    static let appName: String = {
+        Bundle.main.object(forInfoDictionaryKey: "CFBundleDisplayName") as? String ??
+        Bundle.main.object(forInfoDictionaryKey: "CFBundleName") as? String ?? "HabitCurrent"
+    }()
+
+    static let small: CGFloat = 8
+    static let medium: CGFloat = 16
+    
+    static let frameV: CGFloat = 240
+    static let frameVh: CGFloat = frameV + small * 4
+    static let frameVi: CGFloat = frameV / 2
+    
+    static let frameH: CGFloat = 140
+    static let frameHw: CGFloat = frameH / 1.5
+    
+    static let shadowRadius: CGFloat = 4
+    static let shadowOffset: CGFloat = shadowRadius / 2
+    
+    static let cornerRadius: CGFloat = 8
+    static let cornerSize = CGSize(width: 8, height: 8)
+    
+    static var photoHeight: CGFloat {
+        UIScreen.main.bounds.height / 2
+    }
+    
+    static var screenWidth: CGFloat {
+        UIScreen.main.bounds.width
+    }
+}
